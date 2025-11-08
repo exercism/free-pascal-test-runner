686b0394675e6acadad0af1c49909eeb88d803d0
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-11-07 22:38:54 -0300

    * add test unhandled-exception

---
+++ bin/sync-assets.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare bindir
+bindir="$(realpath "$(dirname "$0")")"
+cd "$bindir" || exit 1
+
+while read -r dest; do
+    echo "Syncing $dest..."
+    rsync \
+        --recursive \
+        --links \
+        --perms \
+        --group \
+        --checksum \
+        --out-format='%i %n' \
+        ../assets/ "$dest"
+done < <(find ../ -maxdepth 3 -type d -path '../tests/*')
